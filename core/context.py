c609b41df27f22665cac53ee7940470653b3de68
unknown <aguikenna@fiberswift.com>
2022-07-11 19:20:40 +0100

    ''

--- core/context.py
+++ core/context.py
@@ -13,15 +13,15 @@ def core(request) :
     ctx['site_name_full'] = "Afflus Trade"
     ctx['support_email'] = "support@afflus-trade.com"
     ctx['site_email'] = "support@afflus-trade.com"
-    ctx['site_phone'] = "+3594858"
+    ctx['site_phone'] = "+3594858454"
     ctx['site_whatsapp_no'] = "+66658656fg6"
     ctx['site_address'] = "No 23 winston road new york"
-    ctx['ltc_wallet_address'] = "Ld7quXs9UXyRqQnxFSqwTqkoiWMCotUGdK"
-    ctx['usdt_bep20_wallet_address'] = "0x1aeeffb9bebfa454682db27ba57e3e6079c401b8"
-    ctx['usdt_trc20_wallet_address'] =  "TR1BVAZHnW8PcEdjhXqx6Bc6AF3aTW7i1X"
-    ctx['eth_wallet_address'] = "0x1aeeffb9bebfa454682db27ba57e3e6079c401b8"
-    ctx['btc_wallet_address'] = "bc1qezcxsgzq8g7sjtzt8vpz90tzpdnwnyqvmsgk68"
-    ctx['bnb_wallet_address'] = "0x1aeeffb9bebfa454682db27ba57e3e6079c401b8"
+    ctx['ltc_wallet_address'] = "ltc1q3spehp5aeunwnf75pnj2ka4h4h207cks09vrfx"
+    ctx['usdt_bep20_wallet_address'] = "0x23c6742837e45c2dcdd169b79d06eeeb6b108b9b"
+    ctx['usdt_trc20_wallet_address'] =  "TNiQErvJZMrnXHDGRyaxu9ZeVAPixk98AX"
+    ctx['eth_wallet_address'] = "0x35A3af01822B37FEb4f1E16319daAd1c208e0F9c"
+    ctx['btc_wallet_address'] = "1MJZZuHVErPeEa87rhBC6MpJyp3zRuPyp9"
+    ctx['bnb_wallet_address'] = "0x23c6742837e45c2dcdd169b79d06eeeb6b108b9b"
     
     return ctx  
 
