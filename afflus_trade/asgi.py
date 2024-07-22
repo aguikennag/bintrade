b97ef8ea2a837db2bdc850e58bbcec0a047c58aa
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-07-22 09:46:09 +0100

    ''

--- afflus_trade/asgi.py
+++ afflus_trade/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nintrend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'afflus_trade.settings')
 
 application = get_asgi_application()
